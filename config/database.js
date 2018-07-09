54565f496f99055cc1346aecfbeb542f698ad44d
Myraha Harmon-Arias <myrahaa@gmail.com>
2018-07-09 12:43:13 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://demo:demodemo1@ds233061.mlab.com:33061/coffee-fullstack' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
